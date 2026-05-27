0674e57d7022f8c0210b7a9db0f03a375badd686
Bridge <bridge@local>
2026-05-28 00:21:57 +0300

    chore(tools): add PowerShell parse checker

---
+++ tools/check_parse.ps1
@@ -0,0 +1,5 @@
+﻿param([string]$Path)
+$errors = $null
+$tokens = $null
+$null = [System.Management.Automation.Language.Parser]::ParseFile($Path, [ref]$tokens, [ref]$errors)
+if ($errors.Count -gt 0) { 'ERR: ' + $errors[0].Message } else { 'OK' }
