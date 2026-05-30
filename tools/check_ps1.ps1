e38b170dc474124a8bf7fdf23b7825761b95977f
Bridge <bridge@local>
2026-05-30 16:08:17 +0300

    chore(tools): remove orphaned check_ps1.ps1 utility

--- tools/check_ps1.ps1
+++
@@ -1,4 +0,0 @@
-﻿param([string]$Path)
-$errs = $null
-$null = [System.Management.Automation.Language.Parser]::ParseFile($Path, [ref]$null, [ref]$errs)
-if ($errs.Count) { 'ERR'; $errs | ForEach-Object { $_.Message } } else { 'OK' }
